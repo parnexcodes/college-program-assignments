fba588670b0fecb25c3a0be6ccc0165ee0607e6b
Paranjay Singh <parnexcodes@gmail.com>
2020-12-21 13:18:34 +0530

    Create temp.c

---
+++ temp.c
@@ -0,0 +1,15 @@
+/*  c to convert temprature given in fehrenhite to celsius */
+
+#include <stdio.h>
+
+int temp(void)
+{
+  float far;
+  printf("Enter fahrenheit value : %f", far);
+  scanf("%f", &far);
+  
+  float cel;
+  cel = (far - 32)*5/9.0;
+  printf("Value of celsius : %0.2f", cel);
+  return 0;
+}
