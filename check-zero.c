633a58c5d543dc36acb7ee8ab1391973ebf1dd57
Paranjay Singh <parnexcodes@gmail.com>
2021-01-09 11:31:34 +0530

    Create check-zero.c

---
+++ check-zero.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter value : \n");
+    scanf("%d", &a);
+
+    if (a == 0)
+    {
+    printf("Your number is zero");
+    }
+    else 
+    {
+        printf("Your number is not zero");
+    }
+    return 0;
+
+}
